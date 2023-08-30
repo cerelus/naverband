871551ba648c5ac37fb4063a07477dfc67391129
cerelus <cerelus3@gmail.com>
2023-08-30 20:38:49 +0900

    feat: Add slide to section_report3
    + refactor: section_report2

--- nb/js/main.js
+++ nb/js/main.js
@@ -1,10 +1,12 @@
 import noticeData from "./data/noticeData.js";
 
 window.addEventListener("load", () => {
+    init();
+
     window.addEventListener("scroll", () => {
         fixGnb();
         showTopBtn();
-        getPercent();
+        updatePercent();
     }); /////// scroll
 
     /* gnb */
@@ -12,8 +14,8 @@ window.addEventListener("load", () => {
     const report1 = document.querySelector(".report1");
     // gnb_상단고정
     function fixGnb() {
-        const report1Pos = report1.getBoundingClientRect().top;
-        report1Pos <= 0 ? header.classList.add("fixed") : header.classList.remove("fixed");
+        const r1Pos = report1.getBoundingClientRect().top;
+        r1Pos <= 0 ? header.classList.add("fixed") : header.classList.remove("fixed");
     } ////// fixGnb
     // gnb_link
     const gnbList = header.querySelectorAll(".gnb li a");
@@ -76,29 +78,60 @@ window.addEventListener("load", () => {
     });
 
     /* report2 */
-    function getPercent() {
+    function updatePercent() {
         const report2 = document.querySelector(".report2");
         const scrollBefore = report2.querySelector(".scroll_before");
-        // 스크롤 한계값: 타겟의 offsetHeight - window의 innerHeight
-        // 스크롤 시작위치: report2 - innerHeight*3/5, 스크롤 종료위치: report2 + innerHeight*1/5
-        let scrollHeight = report2.offsetHeight - window.innerHeight / 5;
+        // 스크롤 이동 한계값 = (report2.offsetHeight - window.innerHeight) + window.innerHeight
+        // 시작위치: report2 - window.innerHeight * 2 / 3
+        // 종료위치: report2 + window.innerHeight * 1 / 3
+        let scrollHeight = report2.offsetHeight;
         // 현재 스크롤 위치
-        let currentHeight = -(report2.getBoundingClientRect().top - window.innerHeight / 5 * 3);
-
-        let scrollPercent = Math.floor(currentHeight / scrollHeight * 100);
-        if (scrollPercent >= 0 && scrollPercent <= 100) {
-            scrollBefore.style.clipPath = `inset(0px 0px ${scrollPercent}% )`;
+        let currentHeight = -(report2.getBoundingClientRect().top - (window.innerHeight / 3) * 2);
+        // 퍼센트 업데이트
+        let currentPercent;
+        if (currentHeight >= 0 && currentHeight <= scrollHeight) {
+            let getPercent = Math.floor((currentHeight / scrollHeight) * 100);
+            currentPercent = getPercent;
+        } else if (currentHeight < 0) {
+            currentPercent = 0;
+        } else {
+            currentPercent = 100;
         }
-    } ////// getPercent
+        scrollBefore.style.clipPath = `inset(0px 0px ${currentPercent}% )`;
+    } ////// updatePercent
 
     /* report3 */
     const report3 = document.querySelector(".report3");
+    const r3Slide = report3.querySelector(".slide_container");
+    const wrapper = report3.querySelector(".slide_container ul");
     const textBallon = report3.querySelectorAll(".slide_wrap .text_wrap");
     textBallon.forEach((ele, idx) => {
         let seq = idx + 1;
         ele.innerHTML += `
             <img src="./img/ballon0${seq}.png" alt="말풍선" class="text_ballon ballon${seq}">
         `;
+    });
+
+    // report3_slide
+    let report3Swiper = new Swiper(".report3Slide", {
+        slidesPerView: 1,
+        speed: 800,
+        spaceBetween: 30,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            1025: {
+                slidesPerView: 3,
+                spaceBetween: 0,
+                allowTouchMove: false,
+            },
+        },
     });
 
     /* report4 */
@@ -166,5 +199,9 @@ window.addEventListener("load", () => {
                 ele.classList.remove("on");
             }
         }); //// click
-    }); //// forEach
+    }); ////// forEach
+
+    function init() {
+        updatePercent();
+    }
 }); ///////// load
