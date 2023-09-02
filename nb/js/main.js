2bb340aba43308e163c8514d33b9690dad47ccf8
cerelus <cerelus3@gmail.com>
2023-09-03 02:07:30 +0900

    Refactor: main.js 코드 분할

--- nb/js/main.js
+++ nb/js/main.js
@@ -1,6 +1,7 @@
 import { updateScrollPos, setSmoothScroll } from "./smoothScroll.js";
 import reportData from "./data/reportData.js";
 import noticeData from "./data/noticeData.js";
+import { fixGnb, showTopBtn, getPercent } from "./common.js";
 
 window.addEventListener("load", () => {
     window.addEventListener("scroll", () => {
@@ -21,59 +22,8 @@ window.addEventListener("load", () => {
         updateScrollPos(window.pageYOffset);
     });
 
-    /* gnb */
-    const header = document.getElementById("header");
-    const report1 = document.querySelector(".report1");
-    // gnb_상단고정
-    function fixGnb() {
-        const r1Pos = report1.getBoundingClientRect().top;
-        r1Pos <= 0 ? header.classList.add("fixed") : header.classList.remove("fixed");
-    } ////// fixGnb
-    // gnb_link
-    const gnbList = header.querySelectorAll(".gnb li a");
-    const pgDownBtn = document.querySelector(".btn_page-down");
-    const eventBtn = document.querySelector(".btn_event");
-
-    function movePg(ele) {
-        event.preventDefault();
-        const secId = ele.getAttribute("href");
-        // section의 절대좌표 구하기
-        const secPos = document.querySelector(secId).getBoundingClientRect().top + window.scrollY;
-        window.scrollTo({ top: secPos, behavior: "smooth" });
-        updateScrollPos(secPos);
-    } ////// movePg
-
-    gnbList.forEach((ele) => {
-        ele.addEventListener("click", () => {
-            movePg(ele);
-            for (let x of gnbList) x.parentElement.classList.remove("on");
-            ele.parentElement.classList.add("on");
-        });
-    }); ////// forEach
-
-    pgDownBtn.addEventListener("click", function () {
-        movePg(this);
-    });
-    eventBtn.addEventListener("click", function () {
-        movePg(this);
-    });
-
-    /* top button */
-    const visual = document.querySelector(".visual");
-    const topBtn = document.querySelector(".btn_top");
-    // click-상단이동
-    topBtn.addEventListener("click", (e) => {
-        e.preventDefault();
-        window.scrollTo({ top: 0, behavior: "smooth" });
-        updateScrollPos(0);
-    });
-    // top버튼 표시
-    function showTopBtn() {
-        const visualPos = visual.getBoundingClientRect().top;
-        visualPos < -120 ? topBtn.classList.add("on") : topBtn.classList.remove("on");
-    } ////// showTopBtn
-
     /* visual */
+    const visual = document.querySelector(".visual");
     const viewBtn = visual.querySelector(".btn_view-video");
     const popupVid = document.querySelector(".popup_video");
     const vlVideoWrap = popupVid.querySelector(".video_wrap");
@@ -110,17 +60,6 @@ window.addEventListener("load", () => {
         scrollBefore.style.clipPath = `inset(0px 0px ${currentPercent}% )`;
     } ////// updateBgPos
 
-    // 퍼센트 계산
-    function getPercent(current, overall) {
-        if (current >= 0 && current <= overall) {
-            return Math.floor((current / overall) * 100);
-        } else if (current < 0) {
-            return 0;
-        } else {
-            return 100;
-        }
-    } ////// getPercent
-
     /* report3 */
     const report3 = document.querySelector(".report3");
     const r3TextWrap = report3.querySelectorAll(".slide_wrap .text_wrap");
@@ -148,9 +87,7 @@ window.addEventListener("load", () => {
         let currentHeight = -scrollStart;
         // 퍼섽트 업데이트
         let currentPercent = distance * (1 - getPercent(currentHeight, scrollHeight) / 100);
-        r3Imgs.forEach((ele) => {
-            ele.style.transform = `translateY(${currentPercent}px)`;
-        });
+        for (let y of r3Imgs) y.style.transform = `translateY(${currentPercent}px)`;
     } ////// moveImg
 
     // swiper slide
@@ -197,17 +134,19 @@ window.addEventListener("load", () => {
     report4.append(r4Scroll);
 
     // 텍스트 위치 변경
-    function moveTxt() {
-        const report4 = document.querySelector(".report4");
-        const r4TxtTop = report4.querySelector(".scroll_wrap ul li:nth-child(1)");
-        const r4TxtBottom = report4.querySelector(".scroll_wrap ul li:nth-child(2)");
+    const r4TxtTop = report4.querySelector(".scroll_wrap ul li:nth-child(1)");
+    const r4TxtBottom = report4.querySelector(".scroll_wrap ul li:nth-child(2)");
+    // 텍스트 이동단위
+    let step = 5;
 
+    function moveTxt() {
         let scrollHeight = report4.offsetHeight + window.innerHeight;
-        let currentHeight = report4.getBoundingClientRect().top - window.innerHeight;
+        let scrollStart = report4.getBoundingClientRect().top - window.innerHeight;
+        let currentHeight = -scrollStart;
 
         let currentPercent = getPercent(currentHeight, scrollHeight);
-        r4TxtTop.style.left = `-${currentPercent * 5}px`;
-        r4TxtBottom.style.right = `-${currentPercent * 5}px`;
+        r4TxtTop.style.left = `-${currentPercent * step}px`;
+        r4TxtBottom.style.right = `-${currentPercent * step}px`;
     } ////// moveTxt
 
     /* report6 */
